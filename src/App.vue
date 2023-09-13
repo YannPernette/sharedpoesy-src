1c5c0a8c70ddea0aabccadc7b46b01a01f3fdcf6
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2023-09-13 17:05:47 +0200

    le test ultime src

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import HelloWorld from './components/HelloWorld.vue'
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <h1>BONJOUR CA MARCHE</h1>
+    <h1>BONJOUR CA MARCHE !</h1>
 
     <div class="wrapper">
       <HelloWorld msg="You did it againand again  !" />
